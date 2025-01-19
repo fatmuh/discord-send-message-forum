8e3c59d5aab6cf3f9a9024ffcab80bc248acde75
Fathur <fatmuhdev@gmail.com>
2025-01-19 12:15:52 +0700

    Add mention thread & change color

--- index.js
+++ index.js
@@ -4,14 +4,17 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildMessageTyping,
-        GatewayIntentBits.GuildMembers
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.MessageContent
     ]
 });
 require('dotenv').config();
 
 const token = process.env.DISCORD_TOKEN; // Ganti dengan token bot Anda
 const roleId = process.env.ROLE_ID; // Ganti dengan ID role yang ingin di-mention
+const roleUpdateId = process.env.ROLE_UPDATE_ID; // Ganti dengan ID role yang ingin di-mention
 const channelId = process.env.CHANNEL_ID; // Ganti dengan ID channel tempat bot akan mengirim pesan
+const channelUpdateId = process.env.CHANNEL_UPDATE_ID; // Ganti dengan ID channel tempat bot akan mengirim pesan
 const forumId = process.env.FORUM_ID; // Ganti dengan ID forum yang ingin dipantau
 
 client.once('ready', () => {
@@ -37,7 +40,7 @@ client.on('threadCreate', async (thread) => {
                 const mentionMessage = `${role} ada garapan baru **${thread.name}** sudah ada di channel diskusi garapan, jangan lupa tinggalin jejak ya`;
                 const embed = new EmbedBuilder()
                     .setAuthor({ name: authorName, iconURL: authorAvatar })
-                    .setColor(0x0099FF) // Warna embed (opsional)
+                    .setColor(0x34d399) // Warna embed (opsional)
                     .setTitle(`${thread.name}`)
                     .setDescription(`https://discord.com/channels/${thread.guild.id}/${thread.id}\n\n${description}`)
                     .addFields(
@@ -64,6 +67,50 @@ client.on('threadCreate', async (thread) => {
         }
     } else {
         console.log('Thread is not in the specified forum.');
+    }
+});
+
+// Event ketika ada pesan baru di thread
+client.on('messageCreate', async (message) => {
+    // Pastikan pesan dikirim di dalam thread
+    if (message.channel.isThread() && message.channel.parentId === forumId) {
+        const channel = client.channels.cache.get(channelUpdateId);
+        if (!channel) {
+            console.error('Channel not found.');
+            return;
+        }
+
+        // Periksa jika pesan memiliki mention ke role tertentu
+        const role = message.guild.roles.cache.get(roleId);
+        if (!role) {
+            console.error('Role not found.');
+            return;
+        }
+
+        if (message.mentions.roles.has(roleUpdateId)) {
+            const authorName = message.author.username;
+            const authorAvatar = message.author.displayAvatarURL({ dynamic: true, size: 128 });
+            const content = message.content;
+            const image = message.attachments.first()?.url || null;
+
+            const embed = new EmbedBuilder()
+                .setAuthor({ name: authorName, iconURL: authorAvatar })
+                .setColor(0x3b82f6) // Warna embed (opsional)
+                .setDescription(`https://discord.com/channels/${message.guild.id}/${message.channel.id}\n\n${content}`)
+                .setFooter({ text: 'Garapan BOT' });
+
+            if (image) {
+                embed.setImage(image);
+            }
+
+            // Kirim pesan ke channel target
+            channel.send({
+                content: `${role} ada update baru dari **${message.channel.name}** dikirim sama **${authorName}**.`,
+                embeds: [embed]
+            })
+                .then(() => console.log('Mention detected and message forwarded!'))
+                .catch(err => console.error('Failed to forward message:', err));
+        }
     }
 });
 
